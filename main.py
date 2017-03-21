8908873946c8d00bd5f1e01b3b28b05bd5eb63f9
Bharat Mukheja <bkmukhej@ncsu.edu>
2017-03-21 17:37:08 -0400

    Altered Rake run method to include string punctuation conversion to space

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-import cPickle;
-import marshal;
-import types;
-import sys;
-import numpy;
+import cPickle
+import marshal
+import types
+import sys
+import numpy
 import os.path, time
 from subprocess import call
+import operator
+import textract
+from RAKE import Rake
 
 tag_vs_files_dictfile = 'tag_vs_files_dictionary.pkl'
 print("Running prepare_commands. . .")
 call(["python","prepare_commands.py"])
-print("Running generate_tag_vs_files. . .")
-call(["python","generate_tag_vs_files.py"])
-
-
-
+#print("Running generate_tag_vs_files. . .")
+#call(["python","generate_tag_vs_files.py"])
 
 
 """
 ::: Something for pruning out the non-existing files :::
 """
 def prune_filedict(dict):
- flag=0
- for (key,value) in dict.items():
-  for filename in value:
-   if(not os.path.isfile(filename)):
-    value.remove(filename)
-    print "---pruned file:",filename
-    flag=1
-    if(len(value)==0):
-     del dict[key]
-     print "--prune tag: ",key
-     flag=1
- if(flag==1):
-  print "++updated dictionary file"
-  pkl_file = open(tag_vs_files_dictfile, 'wb')
-  cPickle.dump(dict, pkl_file)
- return dict
+    flag=0
+    for (key,value) in dict.items():
+        for filename in value:
+            if(not os.path.isfile(filename)):
+                value.remove(filename)
+                print "---pruned file:",filename
+                flag=1
+                if(len(value)==0):
+                    del dict[key]
+                    print "--prune tag: ",key
+                    flag=1
+    if(flag==1):
+        print "++updated dictionary file"
+        pkl_file = open(tag_vs_files_dictfile, 'wb')
+        cPickle.dump(dict, pkl_file)
+    return dict
 
-
-
-
-
-
-
+"""Directory Scan"""
+def scandir(inp, func):
+    if inp==None:
+        inp = ['.']
+    for dir in inp:
+        func([path+'/'+item for path,subdir,files in os.walk(dir) for item in files])
+    return
 
 #read tag_vs_files dictionary
 pkl_file = open(tag_vs_files_dictfile, 'rb')
@@ -55,24 +56,30 @@ commands = cPickle.load(pkl_file)
 
 #read user commands
 while(True):
-	inp = raw_input('tagfs#: ')
-	inp = inp.strip()
-	inp = inp.split(' ');
-        if(inp[0] in commands):
-	 func = types.FunctionType(marshal.loads(commands[inp[0]]), globals(), "something")
-         try:
-          if(len(inp)>1):
-           func(inp[1:])
-          else:
-	   func()
-         except:
-           print "Internal Error, is your syntax correct?"
-        elif(inp[0]=='quit' or inp[0]=='q'):
-	 print "Exiting!"
-         break;
-	elif(inp[0]=='prune'):
-	 tag_vs_files = prune_filedict(tag_vs_files);
-        elif(inp[0]==''):
-         continue
+    inp = raw_input('tagfs#: ')
+    inp = inp.strip()
+    inp = inp.split(' ')
+    if inp[0]=='scandir':
+        scandir(inp[1:],func = types.FunctionType(marshal.loads(commands['autotag']), globals(), "something"))
+    elif(inp[0] in commands):
+        func = types.FunctionType(marshal.loads(commands[inp[0]]), globals(), "something")
+        #try:
+        if(len(inp)>1):
+            if inp[0]=='multitag':
+                func([' '.join(inp[1:])])
+            else:
+                func(inp[1:])
         else:
-         print "Command not found!"
+            print "Running function"
+            func()
+            #except:
+            #print "Internal Error, is your syntax correct?"
+    elif(inp[0]=='quit' or inp[0]=='q'):
+        print "Exiting!"
+        break;
+    elif(inp[0]=='prune'):
+        tag_vs_files = prune_filedict(tag_vs_files);
+    elif(inp[0]==''):
+        continue
+    else:
+        print "Command not found!"
